a69e96ded5e9351e3af041068dc852f3e73dba1b
filbertik <aton2019zvit@gmail.com>
2026-08-25 08:14:52 +0300

    use types in servises

--- src/services/weatherApi.ts
+++ src/services/weatherApi.ts
@@ -1,8 +1,12 @@
+import type { CurrentWeather } from "../types/weather";
+
 const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-export const getCurrentWeather = async (city: string) => {
+export const getCurrentWeather = async (
+  city: string,
+): Promise<CurrentWeather> => {
   const response = await fetch(
     `${BASE_URL}/weather?q=${encodeURIComponent(
       city,
@@ -15,3 +19,21 @@ export const getCurrentWeather = async (city: string) => {
 
   return response.json();
 };
+
+// const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+// const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+// export const getCurrentWeather = async (city: string) => {
+//   const response = await fetch(
+//     `${BASE_URL}/weather?q=${encodeURIComponent(
+//       city,
+//     )}&appid=${API_KEY}&units=metric&lang=uk`,
+//   );
+
+//   if (!response.ok) {
+//     throw new Error("Місто не знайдено");
+//   }
+
+//   return response.json();
+// };
