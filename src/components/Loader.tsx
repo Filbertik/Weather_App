733d085cb229fb9a8824ff6c64a9652b5f6c8434
filbertik <aton2019zvit@gmail.com>
2026-08-27 14:47:02 +0300

    loader content

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader" />
+      <p>Loading weather...</p>
+    </div>
+  );
+};
+
+export default Loader;
