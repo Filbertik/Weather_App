c7cdce32d9c7ae8c54a101662adc6a3ea6be7c9e
filbertik <aton2019zvit@gmail.com>
2026-08-28 08:30:39 +0300

    edit api

--- src/services/weatherApi.ts
+++ src/services/weatherApi.ts
@@ -1,4 +1,4 @@
-import type { CurrentWeather } from "../types/weather";
+import type { CurrentWeather, WeatherForecast } from "../types/weather";
 
 const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
@@ -20,11 +20,31 @@ export const getCurrentWeather = async (
   return response.json();
 };
 
+export const getWeatherForecast = async (
+  city: string,
+): Promise<WeatherForecast> => {
+  const response = await fetch(
+    `${BASE_URL}/forecast?q=${encodeURIComponent(
+      city,
+    )}&appid=${API_KEY}&units=metric&lang=uk`,
+  );
+
+  if (!response.ok) {
+    throw new Error("Не вдалося отримати прогноз");
+  }
+
+  return response.json();
+};
+
+// import type { CurrentWeather } from "../types/weather";
+
 // const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
 // const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-// export const getCurrentWeather = async (city: string) => {
+// export const getCurrentWeather = async (
+//   city: string,
+// ): Promise<CurrentWeather> => {
 //   const response = await fetch(
 //     `${BASE_URL}/weather?q=${encodeURIComponent(
 //       city,
@@ -37,3 +57,21 @@ export const getCurrentWeather = async (
 
 //   return response.json();
 // };
+
+// // const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+// // const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+// // export const getCurrentWeather = async (city: string) => {
+// //   const response = await fetch(
+// //     `${BASE_URL}/weather?q=${encodeURIComponent(
+// //       city,
+// //     )}&appid=${API_KEY}&units=metric&lang=uk`,
+// //   );
+
+// //   if (!response.ok) {
+// //     throw new Error("Місто не знайдено");
+// //   }
+
+// //   return response.json();
+// // };
