3335e9667c16ca6b5af3b2224dc4c4d40a5d6489
filbertik <aton2019zvit@gmail.com>
2026-09-07 11:04:11 +0300

    user temperature

---
+++ src/hooks/useTemperatureUnit.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+import type { TemperatureUnit } from "../components/UnitToggle";
+
+const STORAGE_KEY = "weather-temperature-unit";
+
+const getSavedTemperatureUnit = (): TemperatureUnit => {
+  const savedUnit = localStorage.getItem(STORAGE_KEY);
+
+  if (savedUnit === "fahrenheit") {
+    return "fahrenheit";
+  }
+
+  return "celsius";
+};
+
+const useTemperatureUnit = () => {
+  const [unit, setUnit] = useState<TemperatureUnit>("celsius");
+
+  useEffect(() => {
+    const savedUnit = getSavedTemperatureUnit();
+
+    setUnit(savedUnit);
+  }, []);
+
+  const changeUnit = (newUnit: TemperatureUnit) => {
+    setUnit(newUnit);
+
+    localStorage.setItem(STORAGE_KEY, newUnit);
+  };
+
+  return {
+    unit,
+    changeUnit,
+  };
+};
+
+export default useTemperatureUnit;
