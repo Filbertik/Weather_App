35338e90a3314a42dcf3292ec03c66cbb2d87427
filbertik <aton2019zvit@gmail.com>
2026-09-09 13:51:21 +0300

    fix temp utils

--- src/utils/temperature.ts
+++ src/utils/temperature.ts
@@ -17,3 +17,23 @@ export const formatTemperature = (
 ): string => {
   return `${Math.round(convertTemperature(temperature, unit))}°`;
 };
+
+// import type { TemperatureUnit } from "../components/UnitToggle";
+
+// export const convertTemperature = (
+//   temperature: number,
+//   unit: TemperatureUnit,
+// ): number => {
+//   if (unit === "fahrenheit") {
+//     return (temperature * 9) / 5 + 32;
+//   }
+
+//   return temperature;
+// };
+
+// export const formatTemperature = (
+//   temperature: number,
+//   unit: TemperatureUnit,
+// ): string => {
+//   return `${Math.round(convertTemperature(temperature, unit))}°`;
+// };
