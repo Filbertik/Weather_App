d6192d3e689439eb521bccbf30c14b155160d762
filbertik <aton2019zvit@gmail.com>
2026-09-09 13:48:54 +0300

    fix temp

--- src/hooks/useTemperatureUnit.ts
+++ src/hooks/useTemperatureUnit.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import type { TemperatureUnit } from "../components/UnitToggle";
 
@@ -15,13 +15,9 @@ const getSavedTemperatureUnit = (): TemperatureUnit => {
 };
 
 const useTemperatureUnit = () => {
-  const [unit, setUnit] = useState<TemperatureUnit>("celsius");
-
-  useEffect(() => {
-    const savedUnit = getSavedTemperatureUnit();
-
-    setUnit(savedUnit);
-  }, []);
+  const [unit, setUnit] = useState<TemperatureUnit>(() => {
+    return getSavedTemperatureUnit();
+  });
 
   const changeUnit = (newUnit: TemperatureUnit) => {
     setUnit(newUnit);
@@ -36,3 +32,42 @@ const useTemperatureUnit = () => {
 };
 
 export default useTemperatureUnit;
+
+// import { useEffect, useState } from "react";
+
+// import type { TemperatureUnit } from "../components/UnitToggle";
+
+// const STORAGE_KEY = "weather-temperature-unit";
+
+// const getSavedTemperatureUnit = (): TemperatureUnit => {
+//   const savedUnit = localStorage.getItem(STORAGE_KEY);
+
+//   if (savedUnit === "fahrenheit") {
+//     return "fahrenheit";
+//   }
+
+//   return "celsius";
+// };
+
+// const useTemperatureUnit = () => {
+//   const [unit, setUnit] = useState<TemperatureUnit>("celsius");
+
+//   useEffect(() => {
+//     const savedUnit = getSavedTemperatureUnit();
+
+//     setUnit(savedUnit);
+//   }, []);
+
+//   const changeUnit = (newUnit: TemperatureUnit) => {
+//     setUnit(newUnit);
+
+//     localStorage.setItem(STORAGE_KEY, newUnit);
+//   };
+
+//   return {
+//     unit,
+//     changeUnit,
+//   };
+// };
+
+// export default useTemperatureUnit;
